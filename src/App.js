0e2af6a07144bda42301018694b9b7b6d6293395
Dakota <dakotacbrown@gmail.com>
2022-05-10 11:45:19 -0500

    Scroll restoration.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ import ReadingfulMoments from './components/ReadingfulMoments';
 import PostUp from './components/PostUp';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/design-portfolio/" element={<Home />}></Route>
-          <Route path="/design-portfolio/readingful-moments/" element={<ReadingfulMoments />}></Route>
-          <Route path="/design-portfolio/postup/" element={<PostUp />}></Route>
-          <Route path="/design-portfolio/fun/" element={<Fun />}></Route>
-          <Route path="/design-portfolio/about/" element={<About />}></Route>
-        </Routes>
-        <Footer />
+      <Navbar />
+      <ScrollToTop />
+      <Routes>
+            <Route path="/design-portfolio/" element={<Home />}></Route>
+            <Route path="/design-portfolio/readingful-moments/" element={<ReadingfulMoments />}></Route>
+            <Route path="/design-portfolio/postup/" element={<PostUp />}></Route>
+            <Route path="/design-portfolio/fun/" element={<Fun />}></Route>
+            <Route path="/design-portfolio/about/" element={<About />}></Route>
+      </Routes>
+      <Footer />
     </Router>
   );
 }
