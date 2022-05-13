e90564560870fe60cbcf226851676834f27b8322
Dakota <dakotacbrown@gmail.com>
2022-05-13 11:32:48 -0500

    About page update

--- src/components/About.js
+++ src/components/About.js
@@ -21,8 +21,7 @@ export default function About() {
                         <h5>(somewhat) fast facts</h5>
                         <br />
                         <p>
-                        &#127474;&#127485; &#8596; &#127482;&#127480; I'm transnational too! Forced migration is scary when you're 4 years old, and it is still an experience that I am processing over two decades later. It is not something I romanticize or "take pride in", though I am an unwavering ally to other migrant babies. <br /><br />
-                        &#127758; As an adult, I've had the privilege to live in Mexico, the US (MA, NH, CO, TX), Ecuador, Brazil, Czechia, and Guatemala. I've also travelled to 20 other countries. Ask me for travel stories - those are free! <br /><br />
+                        &#127758; I've had the privilege to live in Mexico, the US (MA, NH, CO, TX), Ecuador, Brazil, Czechia, and Guatemala. I've also travelled to 20 other countries. Ask me for travel stories! <br /><br />
                         &#128568; I'm the luckiest parent to the most perfect angel, Taco Cat. I aim to be as fierce about consent as she is - touch her without it and the claws come out &#128150;. <br /><br />
                         &#127793; I'm also a budding plant parent. I took a leaf of faith and started an herb garden - and so should you. I'm rooting for you! <br /><br />
                         &#129343; Scuba diving is the closet I've gotten to feel magic. Were it not for global pandemics, my life would be measured by the time between dives. <br /><br />
